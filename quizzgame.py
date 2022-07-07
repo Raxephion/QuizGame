e548b362ec6005c7c9f63fdd2bd4b2e1cc91fbcb
Raxephion <Raxephion@gmail.com>
2022-07-07 15:17:55 +0200

    Add files via upload

---
+++ quizzgame.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 15:15:32 2022
+
+@author: Pierre-Henri Rossouw
+
+60 Apps in 60 Days Challenge - App #9: Quizz Game
+"""
+
+def check_guess(guess, answer):
+    global score
+    still_guessing = True
+    attempt = 0
+    while still_guessing and attempt < 3:
+        if guess.lower() == answer.lower():
+            print("Correct Answer")
+            score = score + 1
+            still_guessing = False
+        else:
+            if attempt < 2:
+                guess = input("Sorry Wrong Answer, try again")
+            attempt = attempt + 1
+    if attempt == 3:
+        print("The Correct answer is ",answer )
+    
+score = 0
+print("Guess the Animal")
+guess1 = input("Which bear lives at the North Pole? ")
+check_guess(guess1, "polar bear")
+guess2 = input("Which is the fastest land animal? ")
+check_guess(guess2, "Cheetah")
+guess3 = input("Which is the larget animal? ")
+check_guess(guess3, "Blue Whale")
+print("Your Score is "+ str(score))
